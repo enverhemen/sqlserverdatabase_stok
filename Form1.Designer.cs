596f12bb5f360f9371345cc1e099b67b94ae4f9f
Enver Hemen <47403613+enverhemen@users.noreply.github.com>
2019-04-16 12:56:08 +0300

    Stok Takip
    Stok takip veri tabanının bir öreniğini içermektedir.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace stok_takip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.txt_fiyat = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(69, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(72, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fiyat";
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_ad.Location = new System.Drawing.Point(143, 91);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_ad.Size = new System.Drawing.Size(259, 20);
+            this.txt_ad.TabIndex = 2;
+            this.txt_ad.Text = "Ürün adını girin";
+            this.txt_ad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_ad.TextChanged += new System.EventHandler(this.txt_ad_TextChanged);
+            // 
+            // txt_fiyat
+            // 
+            this.txt_fiyat.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_fiyat.Location = new System.Drawing.Point(143, 132);
+            this.txt_fiyat.Name = "txt_fiyat";
+            this.txt_fiyat.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txt_fiyat.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_fiyat.Size = new System.Drawing.Size(259, 20);
+            this.txt_fiyat.TabIndex = 3;
+            this.txt_fiyat.Text = "Fiyat girin";
+            this.txt_fiyat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_fiyat.TextChanged += new System.EventHandler(this.txt_fiyat_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(523, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(523, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(72, 253);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(556, 172);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = ".....";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = ".....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_fiyat);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.TextBox txt_fiyat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stok_takip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection
+            ("Data Source = DESKTOP-HI0L6HS\\SQLEXPRESS; Initial Catalog=stok; Integrated Security=True; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False");
+        SqlCommand komut = new SqlCommand();
+        SqlDataAdapter adaptor = new SqlDataAdapter();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            if ((baglanti.State == ConnectionState.Closed)) baglanti.Open();
+            komut = new SqlCommand("SELECT * FROM urunler", baglanti);
+            SqlDataReader reader = komut.ExecuteReader();
+            reader.Read();
+
+            if (reader.HasRows)
+            {
+                txt_ad.Text = reader["ad"].ToString();
+                txt_fiyat.Text = reader.GetDecimal(4).ToString();
+            }
+            reader.Close();
+            adaptor.SelectCommand = new SqlCommand
+                ("select ad,fiyat from urunler", baglanti);
+            adaptor.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            baglanti.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                if (baglanti.State == ConnectionState.Closed) baglanti.Open();
+                ds.Clear();
+                SqlCommand komut = new SqlCommand
+                    ("INSERT INTO urunler (ad, fiyat) VALUES ('" + txt_ad.Text + "','" + txt_fiyat.Text + "')", baglanti);
+                komut.ExecuteNonQuery();
+                dataGridView1.Update();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Eklendi!");
+          
+            }
+            catch (Exception ex)
+
+            {
+                MessageBox.Show(ex.Message);
+                baglanti.Close();
+
+            }
+        }
+
+        private void txt_ad_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txt_fiyat_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                if (baglanti.State == ConnectionState.Closed) baglanti.Open();
+                ds.Clone();
+                SqlCommand komut = new SqlCommand
+                    ("UPDATE urunler SET ad = '" + txt_ad.Text + "', fiyat='" + txt_fiyat.Text + "' WHERE ad = '" + txt_ad.Text + "'", baglanti);
+                komut.ExecuteNonQuery();
+                dataGridView1.Update();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Güncellendi!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                baglanti.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                if (baglanti.State == ConnectionState.Closed) baglanti.Open();
+                ds.Clear();
+                SqlCommand komut = new SqlCommand
+                    ("DELETE FROM urunler WHERE ad='" + txt_ad.Text + "'", baglanti);
+                komut.ExecuteNonQuery();
+                dataGridView1.Update();
+                dataGridView1.Refresh();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Silindi!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                baglanti.Close();
+            }
+        }
+    }
+}
